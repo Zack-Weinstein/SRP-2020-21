d8739dfe6016b57dccf0f1840de396eb3a157831
Zachary Weinstein <weinstein.zack@gmail.com>
2021-01-08 12:00:33 -0800

    png test

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ camera.resolution = (256, 144)
 ph = 0
 
 while(True):
-    camera.capture('/home/pi/Desktop/photo_%s.jpg' % ph)
+    camera.capture('/home/pi/Desktop/photo_%s.png' % ph)
     ph = ph + 1
