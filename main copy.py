ac5f04bfbc8d9d4cb2539f09c4d120b105ed41c6
Zachary Weinstein <weinstein.zack@gmail.com>
2021-01-10 01:10:18 -0800

    I hope this works

--- main copy.py
+++ main copy.py
@@ -5,7 +5,7 @@ import shutil
 import cv2
 from picamera import PiCamera
 from picamera.array import PiRGBArray
-#import numpy as np
+import numpy as np 
 from time import sleep
 
 sensitivity = 3
@@ -17,8 +17,8 @@ resX = 1920
 resY = 1080
 aResX = 256
 aResY = 144
-chunkX = aResX / 4
-chunkY = aResY / 2
+chunkX = resX / 4
+chunkY = resY / 2
 chunkPixs = chunkX * chunkY
 camera = PiCamera()
 camera.resolution = (resX, resY)
@@ -27,14 +27,7 @@ ph = 0
 vi = 0
 newDir = True
 dirNum = 1
-LCapture = PiRGBArray(camera, size=(resX, resY))
-SCapture = PiRGBArray(camera, size=(aResX, aResY))
-LStream = camera.capture_continuous(LCapture, format="bgr", use_video_port=True)
-SStream = camera.capture_continuous(SCapture, format="bgr", use_video_port=True, splitter_port=2, resize=(aResX, aResY))
-LNext = LStream.__next__()
-SNext = SStream.__next__()
-Limg = LNext.array
-Simg = SNext.array
+rawCapture = PiRGBArray(camera, size=(resX, resY))
 while (newDir):
     if os.path.isdir('/home/pi/Desktop/%s' % dirNum):
         dirNum = dirNum + 1
@@ -57,10 +50,7 @@ def updateValues():
         chunkData[data] = 0
     #camera.resolution = (resX, resY)
     #camera.capture('/home/pi/Desktop/Analysis/photo.jpg')
-    LNext = LStream.__next__()
-    SNext = SStream.__next__()
-    Limg = LNext.array
-    Simg = SNext.array
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     print()
     for chunkNum in range(0, 8):
         print("chunk %s" % chunkNum)
@@ -74,7 +64,7 @@ def updateValues():
         Yend = Ystart + chunkY
         for piX in range(int(Xstart), int(Xend)):
             for piY in range(int(Ystart), int(Yend)):
-                chunkData[chunkNum] = chunkData[chunkNum] + Simg[piY, piX]
+                chunkData[chunkNum] = chunkData[chunkNum] + img[piY, piX]
         chunkData[chunkNum] = round((chunkData[chunkNum] / chunkPixs) * (100 / 255))
         print(chunkData[chunkNum])
 
@@ -90,10 +80,9 @@ def evaluateData():
 def saveMedia(type):
     global ph
     global vi
-    global Limg
     if type == "photo":
         #shutil.move('/home/pi/Desktop/Analysis/photo_%s.jpg' % ph, '/home/pi/Desktop/%s' % dirNum)
-        cv2.imwrite('/home/pi/Desktop/%s/photo_%s.png' % (dirNum, ph), Limg)
+        cv2.imwrite('/home/pi/Desktop/%s/photo_%s.png' % (dirNum, ph), image)
         ph = ph + 1
     if type == "video":
         shutil.move('/home/pi/Desktop/Analysis/video_%s.h264' % vi, '/home/pi/Desktop/%s' % dirNum)
@@ -103,12 +92,12 @@ def saveMedia(type):
 def resetCache():
     shutil.rmtree('/home/pi/Desktop/Analysis')
     os.mkdir('/home/pi/Desktop/Analysis')
-    LCapture.truncate(0)
-    SCapture.truncate(0)
+    rawCapture.truncate()
 
 resetCache()
-while True: 
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):    
     #capture("photo", 1)
+    image = frame.array
     updateValues()
     evaluateData()
     if motionFlag:
