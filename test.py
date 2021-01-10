8906a9f7e760f1b4b4afe8a04847ae99cdb778ca
Zachary Weinstein <weinstein.zack@gmail.com>
2021-01-10 02:23:06 -0800

    organization

--- test.py
+++
@@ -1,8 +0,0 @@
-from picamera import PiCamera
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-ph = 0
-
-while(True):
-    camera.capture('/home/pi/Desktop/photo_%s.png' % ph)
-    ph = ph + 1
