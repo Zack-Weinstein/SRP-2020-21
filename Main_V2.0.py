dad3aa238bf652a55eb0c1a34b3ca3b71a95932c
Zachary Weinstein <weinstein.zack@gmail.com>
2021-01-11 17:34:53 -0800

    speedy as fuck boy

--- Main_V2.0.py
+++ Main_V2.0.py
@@ -12,7 +12,7 @@ import time
 h = 1
 
 sensitivity = 1.5
-evalInterval = 2
+evalInterval = 0.1
 motionFlag = False
 chunks = ["aa", "ab", "ac", "ad", "ba", "bb", "bc", "bd",]
 chunkData = [0, 0, 0, 0, 0, 0, 0, 0]
